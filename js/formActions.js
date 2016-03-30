852dd12a0b538262f5260a6f1b9b7b4ec569f8c8
Pete Burch <peteburch223@gmail>
2016-03-30 18:07:43 +0100

    including weather request

--- js/formActions.js
+++ js/formActions.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
   var thermostat = new Thermostat();
+  navigator.geolocation.getCurrentPosition(onPositionUpdate);
+
   updateDisplay(thermostat);
+
+
+  function onPositionUpdate(position) {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      $.ajax({
+          url : "http://api.wunderground.com/api/ApiId/geolookup/conditions/q/"+lat+","+lon+".json",
+          dataType : "jsonp",
+          success : function(parsed_json) {
+              var location = parsed_json['location']['city'];
+              var temp_f = parsed_json['current_observation']['temp_f'];
+              alert("Current temperature in " + location + " is: " + temp_f);
+          }
+      });
+  }
+
 
   $("#powersaving-on").click(function(event){
     thermostat.psmOn();
@@ -21,11 +39,15 @@ $(document).ready(function(){
     thermostat.down();
     updateDisplay(thermostat);
   });
+
   $("#temperature-reset").click(function(){
     thermostat.resetTemperature();
     updateDisplay(thermostat);
   });
 
+  $("#get-current-weather").click(function(){
+    updateWeather($("#get-current-weather").val());
+  });
 
   function updateDisplay(){
     if(thermostat.power_save){
@@ -37,6 +59,28 @@ $(document).ready(function(){
     $("#temperature").text(thermostat.temperature);
   }
 
+  function updateWeather(city){
+    URL_BASE = 'http://api.openweathermap.org/data/2.5/weather';
+    API_KEY = "c588cd4dbd4ef528c87265572854b0eb";
+    url = URL_BASE + "?q=" + city + "&APPID=" + API_KEY;
+    console.log(url);
+    $.getJSON(url, function(data){
+      displayWeather(data);
+      displayWeatherIcon(data);
+    });
+  }
 
+  function displayWeatherIcon(data){
+    ICON_BASE = "http://openweathermap.org/img/w/";
+    url = ICON_BASE + data.weather[0].icon + ".png";
+    $("#weather-icon").attr("src",url);
+  }
+
+  function displayWeather(data){
+    weatherDescription = data.weather[0].main;
+    weatherTemperature = Math.round(data.main.temp - 273);
+    weatherText = weatherDescription + ": " + weatherTemperature + "&deg;C"
+    $("#weather-text").html(weatherText);
+  }
 
 });

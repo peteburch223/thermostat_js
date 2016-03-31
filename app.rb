8cfe1b38a67c28e5c72448b1806b96bd6d8fe4e4
Pete Burch <peteburch223@gmail>
2016-03-31 11:32:52 +0100

    not done much

--- app.rb
+++ app.rb
@@ -2,10 +2,21 @@ require 'sinatra/base'
 
 class Thermostat < Sinatra::Base
 
+
+
 set :public_folder, 'public'
   get '/' do
     redirect '/index.html'
   end
+
+  post '/temperature' do
+    # receiv
+  end
+
+  get '/temperature' do
+    # receiv
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0

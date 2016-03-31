fcaaef349e407cd3c0b815da5504efd8e735fd0a
phoebe <phoebe.greig@gmail.com>
2016-03-31 11:18:51 +0100

    sinatra

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+
+class Thermostat < Sinatra::Base
+
+set :public_folder, 'public'
+  get '/' do
+    redirect '/index.html'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
